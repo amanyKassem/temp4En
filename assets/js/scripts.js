f7c332e7488dbd756eb23c3b929f06f53c043275
amanyKassem <amany.kassem95@gmail.com>
2021-08-01 11:18:07 +0200

    en temp

---
+++ assets/js/scripts.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+
+
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 50) {
+            $(".normalHeader").addClass("changeBg");
+        } else {
+            //remove the background property so it comes transparent again
+            $(".normalHeader").removeClass("changeBg");
+        }
+    });
+
+    // toggle menu
+    $("header .toggle").click(function () {
+        $(".overlay").css({
+            "transform": "scaleX(1)"
+        });
+
+        $(".menu").addClass('ulDir');
+
+    });
+
+    $("header .overlay").click(function () {
+        $(this).removeAttr("style");
+        $(".menu").removeClass("ulDir");
+    });
+
+
+
+    //scroll top
+    var scrollButton = $("#scroll-top");
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 700) {
+            scrollButton.fadeIn(1000);
+        } else {
+            scrollButton.fadeOut(1000);
+        }
+    });
+
+    //click to scroll top
+    scrollButton.click(function () {
+        $('html,body').animate({scrollTop: 0}, 600);
+    });
+
+});
+
+/* loading screen */
+$(window).on('load', function () {
+
+    $(".layer-preloader").fadeOut(700, function () {
+
+        $(".lds-dual-ring").delay(1000).fadeOut(700);
+
+        $("body").css("overflow-y", "auto");
+
+    });
+
+});
